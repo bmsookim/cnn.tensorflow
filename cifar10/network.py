90f91ec97197fa98f9ca9a65c52137746847e85f
meliketoy <meliketoy@gmail.com>
2016-10-20 10:29:43 +0900

    Updated VGG network

--- cifar10/network.py
+++ cifar10/network.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import config as cf
 import functions as F
+from tensorflow.python.training import moving_averages
 
 class BasicConvNet(object):
     def __init__(self, image_w=cf.w, image_h=cf.h, channels=cf.channels, num_classes=10):
@@ -10,6 +11,8 @@ class BasicConvNet(object):
         self._batch_size = cf.batch_size # define the batch size of mini-batch training.
         self._channels = cf.channels # define the number of channels. ex) RGB = 3, GrayScale = 1, FeatureMap = 50
         self._num_classes = num_classes # define the number of classes for final classfication
+        self.mode = cf.mode
+        self._extra_train_ops = []
 
         # define the basic options for tensorflow session : restricts allocation of GPU memory.
         gpu_options = tf.GPUOptions(allow_growth = True)
@@ -22,7 +25,7 @@ class BasicConvNet(object):
         self._global_step = tf.Variable(0, tf.int64, name="global_step") # saves the global step of training.
 
         # loss calculation & update
-        self._logits = self._inference(self._images, self._keep_prob) # prediction
+        self._logits = self._inference(self._images) # prediction
         self._avg_loss = self._loss(self._labels, self._logits) # difference between prediction & actual label.
         self._train_op = self._train(self._avg_loss) # back propagate the loss.
         self._accuracy = F.accuracy_score(self._labels, self._logits) # get the accuracy of given prediction batch.
@@ -31,7 +34,39 @@ class BasicConvNet(object):
         self._saver = tf.train.Saver(tf.all_variables())
         self._session.run(tf.initialize_all_variables())
 
+    def _batch_norm(self, name, x):
+        """Batch normalization."""
+        with tf.variable_scope(name):
+            params_shape = [x.get_shape()[-1]]
+            beta = tf.get_variable('beta', params_shape, tf.float32,
+              initializer=tf.constant_initializer(0.0, tf.float32))
+            gamma = tf.get_variable('gamma', params_shape, tf.float32,
+              initializer=tf.constant_initializer(1.0, tf.float32))
+
+            if self.mode == 'train':
+                mean, variance = tf.nn.moments(x, [0, 1, 2], name='moments')
+
+                moving_mean = tf.get_variable('moving_mean', params_shape, tf.float32,
+                   initializer=tf.constant_initializer(0.0, tf.float32),trainable=False)
+                moving_variance = tf.get_variable('moving_variance', params_shape, tf.float32,
+                   initializer=tf.constant_initializer(1.0, tf.float32),trainable=False)
+                self._extra_train_ops.append(moving_averages.assign_moving_average(moving_mean, mean, 0.9))
+                self._extra_train_ops.append(moving_averages.assign_moving_average(moving_variance, variance, 0.9))
+            else:
+                mean = tf.get_variable('moving_mean', params_shape, tf.float32,
+                   initializer=tf.constant_initializer(0.0, tf.float32), trainable=False)
+                variance = tf.get_variable('moving_variance', params_shape, tf.float32,
+                   initializer=tf.constant_initializer(1.0, tf.float32), trainable=False)
+
+                tf.histogram_summary(mean.op.name, mean)
+                tf.histogram_summary(variance.op.name, variance)
+            # elipson used to be 1e-5. Maybe 0.001 solves NaN problem in deeper net.
+            y = tf.nn.batch_normalization(x, mean, variance, beta, gamma, 0.001)
+            y.set_shape(x.get_shape())
+            return y
+
     def fit(self, X, y):
+        self.mode = 'train'
         for i in range(0, len(X), self._batch_size): # read whole training dataset by batch size interpolation.
             batch_images, batch_labels = X[i:i+self._batch_size], y[i:i+self._batch_size]
 
@@ -39,7 +74,6 @@ class BasicConvNet(object):
             feed_dict = {
                     self._images : batch_images,
                     self._labels : batch_labels,
-                    self._keep_prob : cf.dropout_rate,
             }
 
             _, acc, train_avg_loss, global_step = self._session.run(
@@ -54,6 +88,7 @@ class BasicConvNet(object):
                 print('Minibatch accuracy: %.2f%%' %(acc*100))
 
     def score(self, X, y):
+        self.mode = 'test'
         total_acc, total_loss = 0, 0
 
         for i in range(0, len(X), self._batch_size):
@@ -62,7 +97,6 @@ class BasicConvNet(object):
             feed_dict = {
                     self._images : batch_images,
                     self._labels : batch_labels,
-                    self._keep_prob : [1, 1, 1, 1, 1],
             }
 
             acc, avg_loss = self._session.run(
@@ -83,7 +117,7 @@ class BasicConvNet(object):
         self._saver.restore(self._session, filepath)
         print("Model restored.")
 
-    def _inferenece(self, X, keep_prob):
+    def _inferenece(self, X):
         pass
 
     def _loss(self, labels, logits):
@@ -103,60 +137,87 @@ class BasicConvNet(object):
         return tf.group(*train_ops)
 
 class vggnet(BasicConvNet):
-    def _inference(self, X, keep_prob):
+    def _inference(self, X):
+        if self.mode == 'train' :
+            keep_prob = [0.9, 0.8, 0.7, 0.6, 0.5]
+        else :
+            keep_prob = [1.0]*5
+
         # Conv_layer 1
         conv = F.conv(X, 64)
-        batch_norm = tf.contrib.layers.batch_norm(conv, scale=True, epsilon=1e-3, is_training=cf.train, decay=0.1)
+        batch_norm = self._batch_norm('bn1', conv)
         relu = F.activation(batch_norm)
-        dropout = F.dropout(relu, keep_prob[0])
+        dropout = F.dropout(relu, keep_prob[0], cf.train)
+
         conv = F.conv(dropout, 64)
-        batch_norm = tf.contrib.layers.batch_norm(conv, scale=True, epsilon=1e-3, is_training=cf.train, decay=0.1)
+        batch_norm = self._batch_norm('bn2', conv)
         relu = F.activation(batch_norm)
-        dropout = F.dropout(relu, keep_prob[0])
-        max_pool = F.max_pool(dropout)
+        dropout = F.dropout(relu, keep_prob[0], cf.train)
+
+        max_pool = F.max_pool(dropout) # 16 x 16
 
         # Conv_layer 2
         conv = F.conv(max_pool, 128)
-        batch_norm = tf.contrib.layers.batch_norm(conv, scale=True, epsilon=1e-3, is_training=cf.train, decay=0.1)
+        batch_norm = self._batch_norm('bn3', conv)
         relu = F.activation(batch_norm)
-        dropout = F.dropout(relu, keep_prob[1])
+        dropout = F.dropout(relu, keep_prob[1], cf.train)
+
         conv = F.conv(dropout, 128)
-        batch_norm = tf.contrib.layers.batch_norm(conv, scale=True, epsilon=1e-3, is_training=cf.train, decay=0.1)
+        batch_norm = self._batch_norm('bn4', conv)
         relu = F.activation(batch_norm)
-        dropout = F.dropout(relu, keep_prob[1])
-        max_pool = F.max_pool(dropout)
+        dropout = F.dropout(relu, keep_prob[1], cf.train)
+
+        max_pool = F.max_pool(dropout) # 8 x 8
 
         # Conv_layer 3
         conv = F.conv(max_pool, 256)
-        batch_norm = tf.contrib.layers.batch_norm(conv, scale=True, epsilon=1e-3, is_training=cf.train, decay=0.1)
+        batch_norm = self._batch_norm('bn5', conv)
         relu = F.activation(batch_norm)
-        dropout = F.dropout(relu, keep_prob[2])
+        dropout = F.dropout(relu, keep_prob[2], cf.train)
+
         conv = F.conv(dropout, 256)
-        batch_norm = tf.contrib.layers.batch_norm(conv, scale=True, epsilon=1e-3, is_training=cf.train, decay=0.1)
+        batch_norm = self._batch_norm('bn6', conv)
         relu = F.activation(batch_norm)
-        dropout = F.dropout(relu, keep_prob[2])
-        max_pool = F.max_pool(dropout)
+        dropout = F.dropout(relu, keep_prob[2], cf.train)
+
+        conv = F.conv(dropout, 256)
+        batch_norm = self._batch_norm('bn7', conv)
+        dropout = F.dropout(relu, keep_prob[2], cf.train)
+
+        max_pool = F.max_pool(dropout) # 4 x 4
 
          # Conv_layer 4
         conv = F.conv(max_pool, 512)
-        batch_norm = tf.contrib.layers.batch_norm(conv, scale=True, epsilon=1e-3, is_training=cf.train, decay=0.1)
+        batch_norm = self._batch_norm('bn8', conv)
         relu = F.activation(batch_norm)
-        dropout = F.dropout(relu, keep_prob[3])
+        dropout = F.dropout(relu, keep_prob[3], cf.train)
+
         conv = F.conv(dropout, 512)
-        batch_norm = tf.contrib.layers.batch_norm(conv, scale=True, epsilon=1e-3, is_training=cf.train, decay=0.1)
+        batch_norm = self._batch_norm('bn9', conv)
         relu = F.activation(batch_norm)
-        dropout = F.dropout(relu, keep_prob[3])
-        max_pool = F.max_pool(dropout)
+        dropout = F.dropout(relu, keep_prob[3], cf.train)
+
+        conv = F.conv(max_pool, 512)
+        batch_norm = self._batch_norm('bn10', conv)
+        relu = F.activation(batch_norm)
+        dropout = F.dropout(relu, keep_prob[3], cf.train)
+
+        max_pool = F.max_pool(dropout) # 2 x 2
 
         # Fully Connected Layer 1
         fc = F.dense(max_pool, 512)
-        batch_norm = tf.contrib.layers.batch_norm(fc, scale=True, epsilon=1e-3, is_training=cf.train, decay=0.1)
+        batch_norm = self._batch_norm('fc', conv)
         relu = F.activation(batch_norm)
-        dropout = F.dropout(relu, keep_prob[4])
+        dropout = F.dropout(relu, keep_prob[4], cf.train)
         h = F.dense(dropout, self._num_classes)
 
         return h
 
     # Overriding for function _train
     def _train(self, avg_loss):
-        return tf.train.AdamOptimizer(1e-3).minimize(avg_loss, aggregation_method=tf.AggregationMethod.EXPERIMENTAL_ACCUMULATE_N, global_step=self._global_step)
+        trainable_variables = tf.trainable_variables()
+        grads = tf.gradients(avg_loss, trainable_variables)
+        optimizer = tf.train.AdamOptimizer(1e-3)#.minimize(avg_loss, aggregation_method=tf.AggregationMethod.EXPERIMENTAL_ACCUMULATE_N, global_step=self._global_step)
+        apply_op = optimizer.apply_gradients(zip(grads, trainable_variables), global_step=self._global_step, name='train_step')
+        train_ops = [apply_op]+self._extra_train_ops
+        return tf.group(*train_ops)
