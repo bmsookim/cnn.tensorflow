3268384271b74da9fcb17035d54e5988c2629124
meliketoy <meliketoy@gmail.com>
2016-10-21 10:47:25 +0900

    updated train file

--- cifar10/train.py
+++ cifar10/train.py
@@ -10,7 +10,7 @@ import batch_load as datasets
 resume = False
 train = cf.train
 
-def run(clf, train):
+def run(clf):
     test_images, test_labels = datasets.load_cifar10(is_train=False)
     records = []
     save_dir = "../models/" + cf.dataset# + ("/%s" % clf.__class__.__name__)
@@ -43,7 +43,7 @@ def run(clf, train):
 
         duration = time.time() - start_time
         time_per = time.time() - epoch_time
-        clf.mode = 'test'
+        clf.mode = 'validation'
         test_accuracy, test_loss = clf.score(test_images, test_labels)
         test_accuracy *= 100
 
@@ -81,4 +81,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--class_name', type=str, default=cf.model, choices=dir(network))
     args = parser.parse_args()
-    run(eval("%s()" % args.class_name), train)
+    run(eval("%s()" % args.class_name))
