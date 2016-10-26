e7a8ac13611e1014e69548a8fe39a63b534e1436
meliketoy <meliketoy@gmail.com>
2016-10-26 17:29:04 +0900

    batch normalization

--- cifar10/network.py
+++ cifar10/network.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import numpy as np
 import config as cf
 import functions as F
-from tensorflow.python.training import moving_averages
 
 class BasicConvNet(object):
     def __init__(self, image_w=cf.w, image_h=cf.h, channels=cf.channels, num_classes=10):
@@ -11,8 +10,6 @@ class BasicConvNet(object):
         self._batch_size = cf.batch_size # define the batch size of mini-batch training.
         self._channels = cf.channels # define the number of channels. ex) RGB = 3, GrayScale = 1, FeatureMap = 50
         self._num_classes = num_classes # define the number of classes for final classfication
-        self.mode = cf.mode # define whether to use moving averages
-        self._extra_train_ops = [] # extra training ops for moving averages
 
         # define the basic options for tensorflow session : restricts allocation of GPU memory.
         gpu_options = tf.GPUOptions(allow_growth = True)
@@ -22,10 +19,11 @@ class BasicConvNet(object):
         self._images = tf.placeholder(tf.float32, shape=[None, self._width, self._height, self._channels])
         self._labels = tf.placeholder(tf.int64, shape=[None])
         self._keep_prob = tf.placeholder(tf.float32)
+        self._is_train = tf.placeholder(tf.bool)
         self._global_step = tf.Variable(0, tf.int64, name="global_step") # saves the global step of training.
 
         # loss calculation & update
-        self._logits = self._inference(self._images, self._keep_prob) # prediction
+        self._logits = self._inference(self._images, self._keep_prob, self._is_train) # prediction
         self._avg_loss = self._loss(self._labels, self._logits) # difference between prediction & actual label.
         self._train_op = self._train(self._avg_loss) # back propagate the loss.
         self._accuracy = F.accuracy_score(self._labels, self._logits) # get the accuracy of given prediction batch.
@@ -34,43 +32,13 @@ class BasicConvNet(object):
         self._saver = tf.train.Saver(tf.all_variables())
         self._session.run(tf.initialize_all_variables())
 
-    def _batch_norm(self, name, x):
-        """Batch normalization."""
-        with tf.variable_scope(name):
-            params_shape = [x.get_shape()[-1]]
-            beta = tf.get_variable('beta', params_shape, tf.float32,
-              initializer=tf.constant_initializer(0.0, tf.float32))
-            gamma = tf.get_variable('gamma', params_shape, tf.float32,
-              initializer=tf.constant_initializer(1.0, tf.float32))
-
-            if self.mode == 'train':
-                mean, variance = tf.nn.moments(x, [0, 1, 2], name='moments')
-
-                moving_mean = tf.get_variable('moving_mean', params_shape, tf.float32,
-                   initializer=tf.constant_initializer(0.0, tf.float32),trainable=False)
-                moving_variance = tf.get_variable('moving_variance', params_shape, tf.float32,
-                   initializer=tf.constant_initializer(1.0, tf.float32),trainable=False)
-                self._extra_train_ops.append(moving_averages.assign_moving_average(moving_mean, mean, 0.9))
-                self._extra_train_ops.append(moving_averages.assign_moving_average(moving_variance, variance, 0.9))
-            else:
-                mean = tf.get_variable('moving_mean', params_shape, tf.float32,
-                   initializer=tf.constant_initializer(0.0, tf.float32), trainable=False)
-                variance = tf.get_variable('moving_variance', params_shape, tf.float32,
-                   initializer=tf.constant_initializer(1.0, tf.float32), trainable=False)
-
-                tf.histogram_summary(mean.op.name, mean)
-                tf.histogram_summary(variance.op.name, variance)
-            # elipson used to be 1e-5. Maybe 0.001 solves NaN problem in deeper net.
-            y = tf.nn.batch_normalization(x, mean, variance, beta, gamma, 0.001)
-            y.set_shape(x.get_shape())
-            return y
-
     def prediction(self, X):
         res = None
         for i in range(0, len(X), self._batch_size):
             batch_images = X[i:i+self._batch_size]
             feed_dict = {
                     self._images: batch_images,
+                    self._is_train : False,
                     self._keep_prob:1.0
                     }
             test_logits = self._session.run(
@@ -87,7 +55,6 @@ class BasicConvNet(object):
 
 
     def fit(self, X, y):
-        self.mode = 'train'
         for i in range(0, len(X), self._batch_size): # read whole training dataset by batch size interpolation.
             batch_images, batch_labels = X[i:i+self._batch_size], y[i:i+self._batch_size]
 
@@ -95,6 +62,7 @@ class BasicConvNet(object):
             feed_dict = {
                     self._images : batch_images,
                     self._labels : batch_labels,
+                    self._is_train : True,
                     self._keep_prob : cf.keep_prob
             }
 
@@ -110,15 +78,16 @@ class BasicConvNet(object):
                 print('Minibatch accuracy: %.2f%%' %(acc*100))
 
     def score(self, X, y):
-        self.mode = 'test'
         total_acc, total_loss = 0, 0
 
+        tf.get_variable_scope().reuse_variables()
         for i in range(0, len(X), self._batch_size):
             batch_images, batch_labels = X[i:i+self._batch_size], y[i:i+self._batch_size]
 
             feed_dict = {
                     self._images : batch_images,
                     self._labels : batch_labels,
+                    self._is_train : False,
                     self._keep_prob : 1.0
             }
 
@@ -140,7 +109,7 @@ class BasicConvNet(object):
         self._saver.restore(self._session, filepath)
         print("Model restored.")
 
-    def _inference(self, X, keep_prob):
+    def _inference(self, X, keep_prob, is_train):
         pass
 
     def _loss(self, labels, logits):
@@ -161,7 +130,7 @@ class BasicConvNet(object):
         return tf.group(*train_ops)
 
 class vggnet(BasicConvNet):
-    def _inference(self, X, keep_prob):
+    def _inference(self, X, keep_prob, is_train):
         # Conv_layer 1
         conv = F.conv(X, 192)
         batch_norm = self._batch_norm('bn1', conv)
@@ -259,11 +228,11 @@ class resnet(BasicConvNet):
         self._k = width
         super(resnet, self).__init__(image_w=cf.w, image_h=cf.h)
 
-    def _residual(self, h, channels, strides, keep_prob):
+    def _residual(self, h, channels, strides, keep_prob, is_train):
         h0 = h
-        h1 = F.conv(F.activation(self._batch_norm('bn1', h0)), channels, strides)
-        h1 = F.dropout(h1, keep_prob, cf.train)
-        h2 = F.conv(F.activation(self._batch_norm('bn2', h1)), channels)
+        h1 = F.conv(F.activation(F._batch_norm(self, 'bn1', h0, is_train)), channels, strides)
+        h1 = F.dropout(h1, keep_prob)
+        h2 = F.conv(F.activation(F._batch_norm(self, 'bn2', h1, is_train)), channels)
         if F.volume(h0) == F.volume(h2):
             h = h0 + h2
         else :
@@ -271,17 +240,17 @@ class resnet(BasicConvNet):
             h = h2 + h4
         return h
 
-    def _inference(self, X, keep_prob):
+    def _inference(self, X, keep_prob, is_train):
         h = F.conv(X, 16)
         for i in range(self._layers):
             with tf.variable_scope('16_layer_%s' %i):
-                h = self._residual(h, channels=16*self._k, strides=1, keep_prob=keep_prob)
+                h = self._residual(h, channels=16*self._k, strides=1, keep_prob=keep_prob, is_train=is_train)
         for channels in [32*self._k, 64*self._k]:
             for i in range(self._layers):
-                with tf.variable_scope(str(channels)+'_layer_%s' %i):
+                with tf.variable_scope(str(channels)+'_layers_%s' %i):
                     strides = 2 if i == 0 else 1
-                    h = self._residual(h, channels, strides, keep_prob)
-        h = F.activation(self._batch_norm('bn3', h))
+                    h = self._residual(h, channels, strides, keep_prob, is_train)
+        h = F.activation(F._batch_norm(self, 'bn', h, is_train))
         h = tf.reduce_mean(h, reduction_indices=[1,2])
         h = F.dense(h, self._num_classes)
 
@@ -297,7 +266,7 @@ class resnet(BasicConvNet):
                 )
 
         # moving averages
-        variable_averages = tf.train.ExponentialMovingAverage(0.9999, self._global_step)
+        variable_averages = tf.train.ExponentialMovingAverage(0.9997, self._global_step)
         tmp_trn_var = tf.trainable_variables()
         update_var = [v for v in tmp_trn_var if v.name != 'global_step:0']
         variable_averages_op = variable_averages.apply(update_var)
@@ -307,8 +276,8 @@ class resnet(BasicConvNet):
         optimizer = tf.train.MomentumOptimizer(learning_rate=lr, momentum=0.9)
         apply_op = optimizer.apply_gradients(zip(grads, trainable_variables),
                 global_step=self._global_step, name='train_step')
-        train_ops = [apply_op]+self._extra_train_ops+[variable_averages_op]
-        return tf.group(*train_ops)
+
+        return tf.group(apply_op, variable_averages_op)
 
 class resnet202(resnet):
     def __init__(self):
