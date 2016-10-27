0bd95f9867131eb26ec375bdf095d52886dae60e
meliketoy <meliketoy@gmail.com>
2016-10-27 16:49:32 +0900

    resume mode

--- cifar10/config.py
+++ cifar10/config.py
@@ -18,7 +18,8 @@ keep_prob = 1-dropout_rate
 lr_decay = 0.0005
 
 # model specification configuration
-model = 'resnet64'
+model = 'resnet28x10'
+resume = False
 
 # step specification configuration
 display_iter = (batch_size*75*float(128/batch_size))
