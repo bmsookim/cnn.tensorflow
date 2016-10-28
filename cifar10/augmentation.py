c2b19b79136c2d609f659a99c6b57d5bc5f215c7
meliketoy <meliketoy@gmail.com>
2016-10-28 17:35:40 +0900

    modified zca and gcn

--- cifar10/augmentation.py
+++ cifar10/augmentation.py
@@ -4,7 +4,6 @@ import urllib
 import cPickle
 import numpy as np
 import config as cf
-from ZCA import *
 
 def unpickle(filename):
     with open(filename, 'rb') as fp:
@@ -15,16 +14,96 @@ def shuffle(images, labels):
     np.random.shuffle(perm)
     return np.asarray(images)[perm], np.asarray(labels)[perm]
 
-def whitening(image, mode='ZCA'):
-    if mode == 'simple' :
-        image -= np.mean(image, axis = 0) # zero centered
-        image /= np.std(image, aixs = 0)  # normalize
-    elif mode == 'ZCA' :
-        image = ZCA(image)
-    else :
-        print ("Wrong augmentation method, no whitening will be occurred")
-    return image
+def global_contrast_normalize(X, scale=1., subtract_mean=True, use_std=False,
+                              sqrt_bias=0., min_divisor=1e-8):
+    """
+    Global contrast normalizes by (optionally) subtracting the mean
+    across features and then normalizes by either the vector norm
+    or the standard deviation (across features, for each example).
+    Parameters
+    ----------
+    X : ndarray, 2-dimensional
+        Design matrix with examples indexed on the first axis and \
+        features indexed on the second.
+    scale : float, optional
+        Multiply features by this const.
+    subtract_mean : bool, optional
+        Remove the mean across features/pixels before normalizing. \
+        Defaults to `True`.
+    use_std : bool, optional
+        Normalize by the per-example standard deviation across features \
+        instead of the vector norm. Defaults to `False`.
+    sqrt_bias : float, optional
+        Fudge factor added inside the square root. Defaults to 0.
+    min_divisor : float, optional
+        If the divisor for an example is less than this value, \
+        do not apply it. Defaults to `1e-8`.
+    Returns
+    -------
+    Xp : ndarray, 2-dimensional
+        The contrast-normalized features.
+    Notes
+    -----
+    `sqrt_bias` = 10 and `use_std = True` (and defaults for all other
+    parameters) corresponds to the preprocessing used in [1].
+    References
+    ----------
+    .. [1] A. Coates, H. Lee and A. Ng. "An Analysis of Single-Layer
+       Networks in Unsupervised Feature Learning". AISTATS 14, 2011.
+       http://www.stanford.edu/~acoates/papers/coatesleeng_aistats_2011.pdf
+    """
+    assert X.ndim == 2, "X.ndim must be 2"
+    scale = float(scale)
+    assert scale >= min_divisor
 
+    # Note: this is per-example mean across pixels, not the
+    # per-pixel mean across examples. So it is perfectly fine
+    # to subtract this without worrying about whether the current
+    # object is the train, valid, or test set.
+    mean = X.mean(axis=1)
+    if subtract_mean:
+        X = X - mean[:, np.newaxis]  # Makes a copy.
+    else:
+        X = X.copy()
+
+    if use_std:
+        # ddof=1 simulates MATLAB's var() behaviour, which is what Adam
+        # Coates' code does.
+        ddof = 1
+
+        # If we don't do this, X.var will return nan.
+        if X.shape[1] == 1:
+            ddof = 0
+
+        normalizers = np.sqrt(sqrt_bias + X.var(axis=1, ddof=ddof)) / scale
+    else:
+        normalizers = np.sqrt(sqrt_bias + (X ** 2).sum(axis=1)) / scale
+
+    # Don't normalize by anything too small.
+    normalizers[normalizers < min_divisor] = 1.
+
+    X /= normalizers[:, np.newaxis]  # Does not make a copy.
+    return X
+
+def flatten_matrix(matrix):
+    vector = matrix.flatten(1)
+    vector = vector.reshape(1, len(vector))
+    return vector
+
+def zca_whitening(inputs):
+    inputs = (inputs - np.mean(inputs))/np.std(inputs)
+    sigma = np.dot(inputs, inputs.T)/inputs.shape[1] #Correlation matrix
+    U,S,V = np.linalg.svd(sigma) #Singular Value Decomposition
+    epsilon = 0.1                #Whitening constant, it prevents division by zero
+    ZCAMatrix = np.dot(np.dot(U, np.diag(1.0/np.sqrt(np.diag(S) + epsilon))), U.T)
+    return np.dot(ZCAMatrix, inputs)   #Data whitening
+
+def whitening(image):
+    vec = flatten_matrix(image)
+    gcn = global_contrast_normalize(vec)
+    zca = zca_whitening(gcn)
+    img = np.reshape(zca, (3, 32, 32))
+    return np.transpose(img, (2, 1, 0))
 
 def resize(dataset, dim):
     resized_dataset = cv2.resize(dataset, dim, interpolation=cv2.INTER_AREA)
