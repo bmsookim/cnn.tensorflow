93d741f181faefeeef4e49c6bf1a78695b6b7e2e
meliketoy <meliketoy@gmail.com>
2016-10-31 15:27:31 +0900

    wide-resnet

--- cifar10/config.py
+++ cifar10/config.py
@@ -18,11 +18,11 @@ keep_prob = 1-dropout_rate
 lr_decay = 0.0005
 
 # model specification configuration
-model = 'resnet28x10'
+model = 'wide_resnet_28x10'
 resume = False
 
 # step specification configuration
 display_iter = (batch_size*75*float(128/batch_size))
-step1 = (60*((50000/batch_size)+1))+1
+step1 = (80*((50000/batch_size)+1))+1
 step2 = (120*((50000/batch_size)+1))+1
 step3 = (160*((50000/batch_size)+1))+1
