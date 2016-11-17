522f2abf327839ddb24f55978f2d0b6505f5c218
bumsoo <meliketoy@gmail.com>
2016-11-17 16:20:45 +0900

    add script files

---
+++ Torch/scripts/cifar10.sh
@@ -0,0 +1,14 @@
+export netType='wide-resnet'
+export dataset='cifar10'
+export save=logs/${dataset}/${netType}/
+export experiment_number=1
+mkdir -p $save
+th main.lua \
+-dataset ${dataset} \
+-netType ${netType} \
+-nGPU 2 \
+-batchSize 128 \
+-top5_display false \
+-depth 28 \
+-widen_factor 10 \
+| tee $save/train_log${experiment_number}.txt
