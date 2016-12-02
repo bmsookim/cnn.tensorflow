90e54fce348b66f3e16add5ffd8d48e3ba1996e1
bumsoo <meliketoy@gmail.com>
2016-12-03 00:07:45 +0900

    best model cifar10

--- Torch/scripts/cifar10.sh
+++ Torch/scripts/cifar10.sh
@@ -3,12 +3,15 @@ export dataset='cifar10'
 export save=logs/${dataset}/${netType}/
 export experiment_number=1
 mkdir -p $save
+mkdir -p modelState
+
 th main.lua \
 -dataset ${dataset} \
 -netType ${netType} \
 -nGPU 2 \
 -batchSize 128 \
 -top5_display false \
--depth 28 \
+-dropout 0.3 \
+-depth 40 \
 -widen_factor 10 \
 | tee $save/train_log${experiment_number}.txt
