85805d2ee80b9617fa4cf025f24dd9278dcc0553
bumsoo <meliketoy@gmail.com>
2016-12-03 01:29:46 +0900

    add testOnly

--- Torch/scripts/cifar10.sh
+++ Torch/scripts/cifar10.sh
@@ -1,6 +1,8 @@
 export netType='wide-resnet'
+export depth=40
+export width=10
 export dataset='cifar10'
-export save=logs/${dataset}/${netType}/
+export save=logs/${dataset}/${netType}-${depth}x${width}/
 export experiment_number=1
 mkdir -p $save
 mkdir -p modelState
@@ -11,7 +13,8 @@ th main.lua \
 -nGPU 2 \
 -batchSize 128 \
 -top5_display false \
+-testOnly false \
 -dropout 0.3 \
--depth 40 \
--widen_factor 10 \
-| tee $save/train_log${experiment_number}.txt
+-depth ${depth} \
+-widen_factor ${width} \
+| tee $save/train_log_${experiment_number}.txt
