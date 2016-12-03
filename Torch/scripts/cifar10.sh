1b91c7917c43ebda0dd0fa038046efee4b9a0ce1
bumsoo <meliketoy@gmail.com>
2016-12-04 07:56:36 +0900

    use test log

--- Torch/scripts/cifar10.sh
+++ Torch/scripts/cifar10.sh
@@ -4,6 +4,7 @@ export width=10
 export dataset='cifar10'
 export save=logs/${dataset}/${netType}-${depth}x${width}/
 export experiment_number=1
+# export CUDA_VISIBLE_DEVICE=0
 mkdir -p $save
 mkdir -p modelState
 
@@ -13,8 +14,7 @@ th main.lua \
 -nGPU 2 \
 -batchSize 128 \
 -top5_display false \
--testOnly false \
--dropout 0.3 \
+-testOnly true \
 -depth ${depth} \
 -widen_factor ${width} \
-| tee $save/train_log_${experiment_number}.txt
+| tee $save/test_log_${experiment_number}.txt
