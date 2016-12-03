0295e4389bd29eced476b49f3e92b255814118ce
bumsoo <meliketoy@gmail.com>
2016-12-04 07:56:48 +0900

    CUDA VISIBLE DEVICES

--- Torch/scripts/cifar100.sh
+++ Torch/scripts/cifar100.sh
@@ -1,14 +1,20 @@
 export netType='wide-resnet'
+export depth=40
+export width=10
 export dataset='cifar100'
-export save=logs/${dataset}/${netType}/
+export save=logs/${dataset}/${netType}-${depth}x${width}/
 export experiment_number=1
+# export CUDA_VISIBLE_DEVICES=1
 mkdir -p $save
+mkdir -p modelState
+
 th main.lua \
 -dataset ${dataset} \
 -netType ${netType} \
 -nGPU 2 \
 -top5_display true \
+-testOnly false \
 -batchSize 128 \
--depth 28 \
--widen_factor 10 \
-| tee $save/train_log${experiment_number}.txt
+-depth ${depth} \
+-widen_factor ${width} \
+| tee $save/train_log_${experiment_number}.txt
