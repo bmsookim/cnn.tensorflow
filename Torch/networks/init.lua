52b3b53a20c6108358a5480970007a288a227944
bumsoo <meliketoy@gmail.com>
2016-12-09 00:04:31 +0900

    reorganized network input for catdog

--- Torch/networks/init.lua
+++ Torch/networks/init.lua
@@ -48,7 +48,7 @@ function M.setup(opt, checkpoint)
    -- optnet is an general library for reducing memory usage in neural networks
    if opt.optnet then
       local optnet = require 'optnet'
-      local imsize = opt.dataset == 'imagenet' and 224 or 32
+      local imsize = (opt.dataset == 'imagenet' or opt.dataset == 'catdog') and 224 or 32
       local sampleInput = torch.zeros(4,3,imsize,imsize):cuda()
       optnet.optimizeMemory(model, sampleInput, {inplace = false, mode = 'training'})
    end
