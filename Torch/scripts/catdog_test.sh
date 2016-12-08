b28309934c7ff9cc5cb5a4f9b545fdd720c7a1cc
bumsoo <meliketoy@gmail.com>
2016-12-09 02:43:32 +0900

    prediction script for catdog tutorial

---
+++ Torch/scripts/catdog_test.sh
@@ -0,0 +1,24 @@
+export netType='wide-resnet'
+export depth=40
+export width=2
+export dataset='catdog'
+export data='gen/catdog'
+export save=logs/${dataset}/${netType}-${depth}x${width}/
+export experiment_number=1
+export CUDA_VISIBLE_DEVICES="0, 1"
+mkdir -p $save
+mkdir -p modelState
+
+th test.lua \
+-dataset ${dataset} \
+-data ${data} \
+-netType ${netType} \
+-nGPU 2 \
+-batchSize 32 \
+-resetClassifier true \
+-nClasses 2 \
+-dropout 0 \
+-top5_display false \
+-testOnly false \
+-depth ${depth} \
+-widen_factor ${width}
