c680c86747179c62029003ea364abee4ffc6d733
bumsoo <meliketoy@gmail.com>
2016-12-10 21:32:44 +0900

    add test file for confidence

--- Torch/test.lua
+++ Torch/test.lua
@@ -1,11 +1,13 @@
+--
+--  Copyright (c) 2016, DMIS
+--  All rights reserved.
+--
 --  (Author) Bumsoo Kim, 2016
 --  Github : https://github.com/meliketoy/ImageRecognition
 --
 --  Korea University, Data-Mining Lab
 --  Image Recognition Torch Implementation
 --
-
-test_path = 'gen/catdog/test/19.jpg'
 
 require 'torch'
 require 'paths'
@@ -32,36 +34,82 @@ local checkpoint, optimState = checkpoints.best(opt)
 local model, criterion = models.setup(opt, checkpoint)
 model:evaluate()
 
-test_image = image.load(test_path)
-interpolation = 'bicubic'
-size = 224
+local function findImages(dir)
+   local imagePaths = torch.CharTensor()
+   local extensionList = {'jpg', 'png', 'jpeg', 'JPG', 'PNG', 'JPEG', 'ppm', 'PPM', 'bmp', 'BMP'}
+   local findOptions = ' -iname "*.' .. extensionList[1] .. '"'
+   for i = 2, #extensionList do
+      findOptions = findOptions .. ' -o -iname "*.' .. extensionList[i] .. '"'
+   end
 
-local w, h = test_image:size(3), test_image:size(2)
+   local f = io.popen('find -L ' .. dir .. findOptions)
 
-if(w<=h and w==size) or (h<=w and h==size) then
-   test_image = test_image
+   local maxLength = -1
+   local imagePaths = {}
+   
+   while true do
+      local line = f:read('*line')
+      if not line then break end
+      local filename = paths.basename(line)
+      local path = dir .. filename
+      table.insert(imagePaths, path)
+      maxLength = math.max(maxLength, #path + 1)
+   end
+
+   f:close()
+
+   local nImages = #imagePaths
+
+   return imagePaths, nImages
 end
 
-if w < h then
-   test_image = image.scale(test_image, size, h/w * size, interpolation)
-else
-   test_image = image.scale(test_image, w/h * size, size, interpolation)
+
+testImagePath, nImages = findImages('gen/catdog/test/')
+count = 0
+
+for i=1, nImages do
+   test_path = testImagePath[i]
+   test_image = image.load(test_path)
+   interpolation = 'bicubic'
+   size = 224
+
+   local w, h = test_image:size(3), test_image:size(2)
+
+   if(w<=h and w==size) or (h<=w and h==size) then
+      test_image = test_image
+   end
+
+   if w < h then
+      test_image = image.scale(test_image, size, h/w * size, interpolation)
+   else
+      test_image = image.scale(test_image, w/h * size, size, interpolation)
+   end
+
+   local w1 = math.ceil((test_image:size(3) -size)/2)
+   local h1 = math.ceil((test_image:size(2) -size)/2)
+
+   test_image = image.crop(test_image, w1, h1, w1+size, h1+size)
+   -- print(test_image:size())
+
+   test_image:resize(1, 3, 224, 224)
+
+   result = model:forward(test_image):float()
+   --print(result)
+
+   exp = torch.exp(result)
+   exp_sum = exp:sum()
+   exp = torch.div(exp, exp_sum)
+   -- print(exp)
+
+   maxs, indices = torch.max(exp, 2)
+
+   if maxs:sum() > 0.8 then
+      count = count + 1
+      print('The prediction for '..test_path..' is '..
+                                 sys.COLORS.red .. labels[indices:sum()] ..
+                                 sys.COLORS.none .. ' by ' .. maxs:sum()
+                                 .. ' confidence')
+   end
 end
 
-local w1 = math.ceil((test_image:size(3) -size)/2)
-local h1 = math.ceil((test_image:size(2) -size)/2)
-
-test_image = image.crop(test_image, w1, h1, w1+size, h1+size)
--- print(test_image:size())
-
-test_image:resize(1, 3, 224, 224)
-
-result = model:forward(test_image):float()
--- print(result)
-
-sigmoid = torch.sigmoid(result)
-print(sigmoid)
-
-maxs, indices = torch.max(sigmoid, 2)
-
-print('The prediction for '..test_path..' is ' .. labels[indices:sum()])
+print(count)
