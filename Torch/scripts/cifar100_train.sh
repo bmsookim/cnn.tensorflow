abb1c136dab353184be9ee58d9b5b34eca111473
bumsoo <meliketoy@gmail.com>
2016-12-15 13:36:17 +0900

    wrn 28x10 train

--- Torch/scripts/cifar100_train.sh
+++ Torch/scripts/cifar100_train.sh
@@ -1,10 +1,9 @@
 export netType='wide-resnet'
-export depth=40
+export depth=28
 export width=10
 export dataset='cifar100'
 export save=logs/${dataset}/${netType}-${depth}x${width}/
 export experiment_number=1
-export CUDA_VISIBLE_DEVICES="0, 1"
 mkdir -p $save
 mkdir -p modelState
 
