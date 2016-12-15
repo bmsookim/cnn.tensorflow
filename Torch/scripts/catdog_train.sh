26bdf9130985e08b05752704b96ce3ce0f5fa4c7
bumsoo <meliketoy@gmail.com>
2016-12-15 13:35:29 +0900

    Added scripts for catdog train

--- Torch/scripts/catdog_train.sh
+++ Torch/scripts/catdog_train.sh
@@ -1,11 +1,10 @@
 export netType='wide-resnet'
-export depth=40
+export depth=50
 export width=2
 export dataset='catdog'
 export data='gen/catdog'
 export save=logs/${dataset}/${netType}-${depth}x${width}/
 export experiment_number=1
-export CUDA_VISIBLE_DEVICES="0, 1"
 mkdir -p $save
 mkdir -p modelState
 
@@ -15,8 +14,6 @@ th main.lua \
 -netType ${netType} \
 -nGPU 2 \
 -batchSize 32 \
--resetClassifier true \
--nClasses 2 \
 -top5_display false \
 -testOnly false \
 -depth ${depth} \
