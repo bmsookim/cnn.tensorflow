46892ea64f9fb6f2914ff72472abd9484994f8a5
bumsoo <meliketoy@gmail.com>
2016-12-21 15:06:38 +0900

    configuration settings

--- Torch/opts.lua
+++ Torch/opts.lua
@@ -35,30 +35,30 @@ function M.parse(arg)
    cmd:option('-nEpochs',         0,       'Number of total epochs to run')
    cmd:option('-epochNumber',     1,       'Manual epoch number (useful on restarts)')
    cmd:option('-batchSize',       128,     'mini-batch size (1 = pure stochastic)')
-   cmd:option('-featureMap',      0,       'final feature map size')
    cmd:option('-display_iter',    75,      'display of training iteration')
    cmd:option('-top5_display',    'false', 'display top5 accuracy')
    cmd:option('-testOnly',        'false', 'Run on validation set only')
    cmd:option('-tenCrop',         'false', 'Ten-crop testing')
    ------------- Checkpointing options ---------------
    cmd:option('-save',            'modelState', 'Directory in which to save checkpoints')
-   cmd:option('-resume',          '', 'Resume from the latest checkpoint in this directory')
+   cmd:option('-resume',          '',           'Resume from the latest checkpoint in this directory')
    cmd:option('-saveLatest',      'false',      'Save the latest file')
    ---------- Optimization options ----------------------
    cmd:option('-LR',              0.1,     'initial learning rate')
    cmd:option('-momentum',        0.9,     'momentum')
-   cmd:option('-weightDecay',     0.0005,  'weight decay')
+   cmd:option('-weightDecay',     5e-4,    'weight decay')
    ---------- Model options ----------------------------------
    cmd:option('-netType',      'wide-resnet', 'Options: resnet | wide-resnet')
-   cmd:option('-depth',        28,       'ResNet depth: 6n+4', 'number')
-   cmd:option('-widen_factor', 10,       'Wide-Resnet width', 'number')
-   cmd:option('-dropout',      0,        'Dropout rate')
-   cmd:option('-shortcutType', '',       'Options: A | B | C')
-   cmd:option('-retrain',      'none',   'fine-tuning, Path to model to retrain with')
-   cmd:option('-optimState',   'none',   'Path to an optimState to reload from')
+   cmd:option('-depth',             28,       'ResNet depth: 6n+4', 'number')
+   cmd:option('-widen_factor',      10,       'Wide-Resnet width', 'number')
+   cmd:option('-dropout',           0,        'Dropout rate')
+   cmd:option('-shortcutType',      '',       'Options: A | B | C')
+   cmd:option('-retrain',         'none',     'fine-tuning, Path to model to retrain with')
+   cmd:option('-optimState',      'none',     'Path to an optimState to reload from')
    ---------- Model options ----------------------------------
    cmd:option('-shareGradInput',  'false', 'Share gradInput tensors to reduce memory usage')
-   cmd:option('-optnet',          'true', 'Use optnet to reduce memory usage')
+   cmd:option('-optnet',          'true',  'Use optnet to reduce memory usage')
+   cmd:option('-convert',         'false', 'Only for converting mode')
    cmd:option('-resetClassifier', 'false', 'Reset the fully connected layer for fine-tuning')
    cmd:option('-nClasses',         0,      'Number of classes in the dataset')
    cmd:text()
@@ -72,6 +72,7 @@ function M.parse(arg)
    opt.optnet = opt.optnet ~= 'false'
    opt.resetClassifier = opt.resetClassifier ~= 'false'
    opt.top5_display = opt.top5_display ~= 'false'
+   opt.convert = opt.convert ~= 'false'
    opt.save = opt.save..'/'..opt.dataset..'/'..opt.netType..'-'..opt.depth..'x'..opt.widen_factor..'/'
    if opt.resume ~= '' then 
        opt.resume = opt.resume..'/'..opt.dataset..'/'..opt.netType..'-'..opt.depth..'x'..opt.widen_factor..'/'
@@ -92,7 +93,6 @@ function M.parse(arg)
       -- Default shortcutType=B and nEpochs=90
       opt.shortcutType = opt.shortcutType == '' and 'B' or opt.shortcutType
       opt.nEpochs = opt.nEpochs == 0 and 90 or opt.nEpochs
-      opt.featureMap = 7
    elseif opt.dataset == 'catdog' then
       -- Handle the most common case of missing -data flag
       local trainDir = paths.concat(opt.data, 'train')
@@ -104,17 +104,14 @@ function M.parse(arg)
       -- Default shortcutType=B and nEpochs=90
       opt.shortcutType = opt.shortcutType == '' and 'B' or opt.shortcutType
       opt.nEpochs = opt.nEpochs == 0 and 90 or opt.nEpochs
-      opt.featureMap = 7
    elseif opt.dataset == 'cifar10' then
       -- Default shortcutType=A and nEpochs=164
       opt.shortcutType = opt.shortcutType == '' and 'A' or opt.shortcutType
       opt.nEpochs = opt.nEpochs == 0 and 200 or opt.nEpochs
-      opt.featureMap = 8
    elseif opt.dataset == 'cifar100' then
       -- Default shortcutType=A and nEpochs=164
       opt.shortcutType = opt.shortcutType == '' and 'A' or opt.shortcutType
       opt.nEpochs = opt.nEpochs == 0 and 200 or opt.nEpochs
-      opt.featureMap = 8
    else
       cmd:error('unknown dataset: ' .. opt.dataset)
    end
